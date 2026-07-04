6e8f842ddcc290892e0de23e97709fa2721a8939
Muhammad <25007658@myrp.edu.sg>
2026-07-05 01:25:39 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const mysql = require('mysql2');
+const app = express();
+ 
+// Create MySQL connection to c237_studentlistapp
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Slenderman11',
+    database: 'c237_studentlistapp'
+});
+ 
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+        return;
+    }
+    console.log('Connected to MySQL database');
+});
+ 
+// Set up view engine
+app.set('view engine', 'ejs');
+// Enable static files
+app.use(express.static('public'));
+// Enable form processing
+app.use(express.urlencoded({extended: false}));
+ 
+// Define routes
+
+// 1. Display all students
+app.get('/', (req, res) => {
+  const sql = 'SELECT * FROM student';
+  connection.query(sql, (error, results) => {
+    if (error) {
+      console.error('Database query error:', error.message); 
+      return res.send('Error Retrieving students'); 
+    }
+    // Render index page with students data array
+    res.render('index', { students: results });
+  });
+});
+
+// 2. Display specific student details
+app.get('/student/:id', (req, res) => {
+  const studentId = req.params.id;
+  const sql = 'SELECT * FROM student WHERE studentId = ?';
+  connection.query(sql, [studentId], (error, results) => {
+    if (error) {
+      console.error('Database query error:', error.message); 
+      return res.send('Error Retrieving student by ID'); 
+    }
+    if (results.length > 0) {
+      res.render('student', { student: results[0] });
+    } else {
+      res.send('Student not found');
+    }
+  });
+});
+
+// 3. Render Add Student form view
+app.get('/addStudent', (req, res) => {
+  res.render('addStudent'); 
+});
+
+// 4. Handle Add Student form submission
+app.post('/addStudent', (req, res) => {
+  const { name, dob, contact, image } = req.body;
+  const sql = 'INSERT INTO student (name, dob, contact, image) VALUES (?, ?, ?, ?)';
+  
+  connection.query(sql, [name, dob, contact, image], (error, results) => {
+    if (error) {
+      console.error("Error adding student:", error);
+      res.send('Error adding student');
+    } else {
+      res.redirect('/');
+    }
+  });
+});
+ 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
